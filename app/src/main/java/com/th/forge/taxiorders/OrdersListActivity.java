94d6ba1ea816430a9edd784f1fa3072c5f7937be
fOrgeth <ivdovin@mail.ru>
2018-11-28 22:30:37 +0300

    init commit

---
+++ app/src/main/java/com/th/forge/taxiorders/OrdersListActivity.java
@@ -0,0 +1,13 @@
+package com.th.forge.taxiorders;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrdersListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return OrdersListFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/th/forge/taxiorders/OrdersListFragment.java
@@ -0,0 +1,10 @@
+package com.th.forge.taxiorders;
+
+import android.support.v4.app.Fragment;
+
+public class OrdersListFragment extends Fragment {
+
+    public static OrdersListFragment newInstance() {
+        return new OrdersListFragment();
+    }
+}
