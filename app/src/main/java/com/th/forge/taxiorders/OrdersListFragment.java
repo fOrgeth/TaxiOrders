a3ea838877f6feb58e0e8b92622af149d0e42182
fOrgeth <ivdovin@mail.ru>
2018-11-29 13:57:09 +0300

    some fixes

--- app/src/main/java/com/th/forge/taxiorders/OrdersListFragment.java
+++ app/src/main/java/com/th/forge/taxiorders/OrdersListFragment.java
@@ -4,16 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.th.forge.taxiorders.temp.SampleOrders;
-
-import java.util.List;
 
 public class OrdersListFragment extends Fragment {
 
--- app/src/main/java/com/th/forge/taxiorders/OrdersRVAdapter.java
+++ app/src/main/java/com/th/forge/taxiorders/OrdersRVAdapter.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import java.util.List;
 
 public class OrdersRVAdapter extends RecyclerView.Adapter<OrdersRVAdapter.OrdersViewHolder> {
